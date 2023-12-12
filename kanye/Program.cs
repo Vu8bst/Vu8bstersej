7ceab898fe4f1b75129eae2996fa7279cc154833
Victor Thomsen <vict756a@mail.hansenberg.dk>
2023-12-12 14:46:31 +0100

    Datatypes

---
+++ kanye/Program.cs
@@ -0,0 +1,145 @@
+﻿
+//System.Threading.Thread.Sleep(10000);
+Console.Clear();
+
+
+//I am a Comment
+
+/*
+I
+Am
+A
+Comment
+*/
+//______________________________________
+
+/*
+using System.IO.Compression;
+
+Console.WriteLine("Kanye West!");
+
+Console.Write("Wake up Mr. West ");
+
+Console.WriteLine("Wake up Mr. West");
+
+Console.WriteLine("Wake up Mr. West, Mr. West");
+
+
+
+ //___________________
+
+//Datatypes: int, float, string, bool
+/*
+int = Helt talt (-5, 4, 6, 5000)
+Float = decimal
+String= tekst
+*/
+
+
+//variables
+
+/*
+int x; //declaring x
+
+x = 4; // instansiating x
+
+int y = 5, z = 6; //declaring and instantsiatiing y
+
+int d = x + y - z * x / z % y;
+
+
+Console.WriteLine("x = " + x + " and y = " + y + " and z = " + z);
+Console.WriteLine("Result of d is = " + d);
+
+float e = 5.5F; 
+
+Console.WriteLine("printing e as a float:" + e);
+
+int f = Convert.ToInt16(e);
+
+Console.WriteLine("printing e as a float:" + f);
+
+string str = "I am a string" + " and so am I ";
+
+Console.WriteLine(str);
+
+
+string str One = "O"
+
+Console.WriteLine("x + y = " + Z);  
+*/
+
+/*
+
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello");
+Console.WriteLine("Victor");
+
+
+
+int x = 4;
+
+int y = 5;
+
+Console.WriteLine(" 4 + 5 is "+(x + y));
+
+
+int x = 5;
+
+int y = 4;
+
+Console.WriteLine(" 5 - 4 is "+(x - y));
+
+
+int x = -1;
+
+int y = 4;
+
+int z = 6;
+
+Console.WriteLine(+(x + y * z));
+
+
+
+Console.WriteLine("The result of (-1 + 4 * 6) = " + (-1+4*6));
+Console.WriteLine("The result is ((35 + 5) % 7) = "+ ((35 + 5) % 7));
+Console.WriteLine("The result is (14 + (-4) * 6 / 11) = "+ (14 + (-4) * 6 / 11));
+Console.WriteLine("The result is (2 + 15 / 6 * 1 - 7 % 2) = "+ (2 + 15 / 6 * 1 - 7 % 2));
+
+
+void SwapNumbers(ref int a, ref int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int x = 5;
+int y = 6;
+
+Console.WriteLine("x: " + x);
+Console.WriteLine("y: " + y);
+
+SwapNumbers(ref x, ref y);
+Console.WriteLine("Swapped");
+
+Console.WriteLine("x: " + x);
+Console.WriteLine("y: " + y);
+
+*/
+
+int = number1;
+ number2, number3;
+
+int number1 = 2;
+
+Console.Write("\nInput the First number to multiply");
+number1 = int.Parse(Console.ReadLine());
+Console.Write("\nInput the Second number to multiply");
+number2 = int.Parse(Console.ReadLine());
+Console.Write("\nInput the Third number to multiply");
+number3 = int.Parse(Console.ReadLine());
+Console.WriteLine( number1 * number2 * number3);
+Console.ReadLine();
