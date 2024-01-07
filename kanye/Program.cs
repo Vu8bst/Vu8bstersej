38e1726ab3f23490eeeb4c1de840e94d47c62544
Victor Thomsen <vict756a@mail.hansenberg.dk>
2024-01-07 22:15:59 +0100

    Kanye is the undisputed GOAT

--- kanye/Program.cs
+++ kanye/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 //System.Threading.Thread.Sleep(10000);
+using System.Security.Cryptography.X509Certificates;
+
 Console.Clear();
 
 
@@ -217,22 +219,200 @@ Console.WriteLine(value: numberX * 10);
 }
 }
 
-*/
+
 
 
 {  
-    string letter1,letter2,letter3;
+    char letter1,letter2,letter3;
   
-    Console.WriteLine("Input letter: ");
-    letter1 = Convert.ToString(Console.ReadLine());
+    Console.WriteLine("Input first letter: ");
+    letter1 = Convert.ToChar(Console.ReadLine());
  
-    Console.WriteLine("Input letter: ");
-    letter2 = Convert.ToString(Console.ReadLine());       
+    Console.WriteLine("Input second letter: ");
+    letter2 = Convert.ToChar(Console.ReadLine());       
  
-    Console.WriteLine("Input letter: ");
-    letter3 = Convert.ToString(Console.ReadLine());
+    Console.WriteLine("Input third letter: ");
+    letter3 = Convert.ToChar(Console.ReadLine());
      
-    Console.WriteLine("{0} {1} {2}",letter3,letter2,letter1);
+    Console.WriteLine("{0} {1} {2}", letter3, letter2, letter1);
    }
+   */
+
+// {
+
+    /*
+
+ int number1,number2,number3;
+double x1, x2;
+
+Console.WriteLine("Input first sqr number: ");
+
+number1 = Convert.ToInt32(Console.ReadLine());
+number2 = Convert.ToInt32(Console.ReadLine());
+number3 = Convert.ToInt32(Console.ReadLine());
+
+double d = number2 * number2 - 4 * number1 * number3;
+
+if (d < 0)
+{
+Console.WriteLine("There is no answer");
+ }
+else if (d == 0)
+{
+    int answer = 0;
+
+}
+else if (d > 0)
+{
+int answer1 = 0;
+int answer2 = 0;
+Console.WriteLine("Both roots are Equal");
+
+
+x1 = (-number2 + Math.Sqrt(d))/(2*number1); 
+x2 = (-number2 - Math.Sqrt(d))/(2*number1);
+
+Console.Write("First  Root Root1= {0}\n",x1);
+Console.Write("Second Root root2= {0}\n",x2);
+
+}
+
+else
+
+{
+Console.Write("Root are imeainary;\nNo Solution. \n\n");
+
+
+}
+
+
+
+
+
+
+
+Console.Write("Input the value of a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the value of b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input the value of c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+var root1 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+var root2 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+
+if (root1 == root2)
+{
+Console.WriteLine("\nBoth roots are equal.");
+Console.WriteLine("First root equals {0}", root1);
+Console.WriteLine("Second root equals {0}", root2);
+}
+else if (double.IsNaN(root1) || double.IsNaN(root2));
+
+ 
+
+int x = 4;
+
+if (x == 4){
+    Console.WriteLine("x is four");
+} else if (x == 3){
+    Console.WriteLine("x is tree");
+} else {
+    Console.WriteLine("everything else");
+}
+
+
+static void Main(string[] args)
+        {
+    double rl,phy,che,ca,total;
+    double per;
+    string nm,div;
+
+    Console.Write("\n\n");
+    Console.Write("Calculate the total, percentage and division to take marks of three subjects:\n");
+    Console.Write("-------------------------------------------------------------------------------");
+    Console.Write("\n\n");
+
+
+    Console.Write("Input the Roll Number of the student :");
+    rl = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Input the Name of the Student :");
+    nm = Console.ReadLine();
+
+    Console.Write("Input  the marks of Physics : ");
+    phy= Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input  the marks of  Chemistry : ");
+    che = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input  the marks of Computer Application : ");
+    ca = Convert.ToInt32(Console.ReadLine());
+
+    total = phy+che+ca;
+    per = total/3.0;
+    if (per>=60)
+	 div="First";
+    else
+	if (per<60&&per>=48)
+	    div="Second";
+	else
+	    if (per<48&&per>=36)
+		div="Pass";
+	     else
+		div="Fail";
+
+       Console.Write("\nRoll No : {0}\nName of Student : {1}\n",rl,nm);
+       Console.Write("Marks in Physics : {0}\nMarks in Chemistry : {1}\nMarks in Computer Application : {2}\n",phy,che,ca);
+       Console.Write("Total Marks = {0}\nPercentage = {1}\nDivision = {2}\n",total,per,div);
+}
+
+int x = 45000;
+
+switch (x){
+    case 1:
+    Console.WriteLine("x is 1");
+    break;
+
+case 2:
+    Console.WriteLine("x is 2");
+    break;
+
+default:
+
+    Console.WriteLine("x is not 1 or 2");
+    break;
+    
+}
+
+*/
+
+for (int i = 0; i < 100; i++){
+
+    if (i == 5){
+        continue;
+    }
+
+        if (i == 7){
+        continue;
+    }
+
+    
+    if (i == 43){
+        continue;
+    }
+
+        if (i == 45){
+        continue;
+    }
+
+        if (i == 67){
+        break;
+        
+    Console.WriteLine(i);
+}
+
+
+
 
 
